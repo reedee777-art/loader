206e066dbc28f455ef8afa5aedb291d812d75ea7
reedee777-art <reedee777@gmail.com>
2025-09-05 12:20:12 +0300

    Update script.user1.js

--- script.user1.js
+++ script.user1.js
@@ -21,16 +21,16 @@
             "https://claimclicks.com/eth/?r=cifer",
             "https://claimclicks.com/doge/?r=cifer",
             "https://claimclicks.com/ltc/?r=cifer",
-           // "https://claimclicks.com/dgb/?r=cifer",
-           // "https://claimclicks.com/dash/?r=cifer",
-          //  "https://claimclicks.com/trx/?r=cifer",
+            "https://claimclicks.com/dgb/?r=cifer",
+            "https://claimclicks.com/dash/?r=cifer",
+            "https://claimclicks.com/trx/?r=cifer",
             "https://claimclicks.com/bnb/?r=cifer",
             "https://claimclicks.com/sol/?r=cifer",
             "https://claimclicks.com/ton/?r=cifer",
-          //  "https://claimclicks.com/ada/?r=cifer",
-          //  "https://claimclicks.com/xrp/?r=cifer",
-          //  "https://claimclicks.com/xlm/?r=cifer",
-          //  "https://claimclicks.com/matic/?r=cifer",
+            "https://claimclicks.com/ada/?r=cifer",
+            "https://claimclicks.com/xrp/?r=cifer",
+            "https://claimclicks.com/xlm/?r=cifer",
+            "https://claimclicks.com/matic/?r=cifer",
             "https://cryptofaucet.one/faucet"
            //  "https://acryptominer.io/user/faucet"
         ];
@@ -77,6 +77,7 @@
         observer.observe(document.body, { childList: true, subtree: true });
     }, 5000); // <-- Задержка 5 секунд
 })();
+
 
 
 
