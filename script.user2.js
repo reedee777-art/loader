835a79b372f57ef836ca0d3ec9ffecea35a9921d
reedee777-art <reedee777@gmail.com>
2026-02-20 17:02:34 +0200

    Update script.user2.js

--- script.user2.js
+++ script.user2.js
@@ -26,7 +26,7 @@
     // Список faucet URL для цикла
     const faucetUrls = [
          'https://claimtrx.com/faucet',
-        'https://faucetpayz.com/faucet',
+       //  'https://faucetpayz.com/faucet',
           'https://feyorra.top/faucet'
         //  'https://cryptofaucet.one/faucet'
     ];
@@ -66,6 +66,7 @@
     }, 1000);
 
 })();
+
 
 
 
