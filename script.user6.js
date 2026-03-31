29f2144c03c56c10dca521914fd2f5e49f69dd18
reedee777-art <reedee777@gmail.com>
2026-03-31 17:39:02 +0300

    Update script.user6.js

--- script.user6.js
+++ script.user6.js
@@ -1,1 +1,22 @@
+// ==UserScript==
+// @name         Crypto Dashboard to linksfly
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Redirect from /app/dashboard to /app/faucet?currency=LTC after 3 seconds
+// @author       You
+// @match        https://*/app/dashboard*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+    // Проверяем, что URL содержит /app/dashboard
+    if (window.location.pathname.includes('/app/dashboard')) {
+        // Ждём 3 секунды
+        setTimeout(function() {
+            // Заменяем /app/dashboard на /app/faucet?currency=LTC
+            var newPath = window.location.pathname.replace('/app/dashboard', '/app/faucet') + '?currency=LTC';
+            window.location.href = window.location.origin + newPath;
+        }, 17000);
+    }
+})();
