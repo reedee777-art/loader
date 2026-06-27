08651dd4d18c1f7c64aec7e2df13718109d2fd00
reedee777-art <reedee777@gmail.com>
2026-06-27 11:18:55 +0300

    Update script.user8.js

--- script.user8.js
+++ script.user8.js
@@ -15,7 +15,7 @@
     const allowedHosts = [
         'faucetpayz.com',
         'earnbitmoon.club',
-             'freebits.xyz',
+            // 'freebits.xyz',
         'cryptoukr.in.ua'
     ];
 
@@ -27,7 +27,7 @@
     // Список сайтов (ТОЛЬКО ДОМЕНЫ)
     const faucetSites = [
         'earnbitmoon.club',
-             'freebits.xyz',
+            // 'freebits.xyz',
         'faucetpayz.com',
         'cryptoukr.in.ua'
     ];
@@ -45,7 +45,7 @@
 
         if (nextHost === 'cryptoukr.in.ua') return 'https://cryptoukr.in.ua/faucet';
         if (nextHost === 'earnbitmoon.club') return 'https://earnbitmoon.club';
-             if (nextHost === 'freebits.xyz') return 'https://freebits.xyz';
+            // if (nextHost === 'freebits.xyz') return 'https://freebits.xyz';
         if (nextHost === 'faucetpayz.com') return 'https://faucetpayz.com/faucet';
     }
 
