12bb3bb86e908188383ef0042bd34fbb8f101143
reedee777-art <reedee777@gmail.com>
2026-08-10 10:05:58 +0300

    Update script.user5.js

--- script.user5.js
+++ script.user5.js
@@ -7,11 +7,23 @@
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
-
 (function() {
     'use strict';
 
+    const EXCLUDED_URLS = [
+        'https://reedee777-art.github.io/loader/loader4a.html'
+    ];
+
     const INTERVAL_SECONDS = 60;
+
+    // Проверяем, не находимся ли мы на исключённой странице
+    const currentURL = location.href.split('?')[0].split('#')[0]; // без query/hash
+    const isExcluded = EXCLUDED_URLS.some(url => currentURL === url);
+
+    if (isExcluded) {
+        console.log('⏸ Автообновление отключено для этой страницы:', currentURL);
+        return;
+    }
 
     setTimeout(function() {
         location.reload();
