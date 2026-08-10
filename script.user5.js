627efc0f04e88669e35f561f3ab05de95af39388
reedee777-art <reedee777@gmail.com>
2026-08-10 09:47:06 +0300

    Update script.user5.js

--- script.user5.js
+++ script.user5.js
@@ -1,1 +1,20 @@
+// ==UserScript==
+// @name         Автообновление страницы каждые 60 сек
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Автоматически перезагружает страницу раз в 60 секунд
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    const INTERVAL_SECONDS = 60;
+
+    setTimeout(function() {
+        location.reload();
+    }, INTERVAL_SECONDS * 1000);
+
+})();
