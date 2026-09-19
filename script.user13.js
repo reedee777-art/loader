f04797543d1143142e50f825f35d1522f25b9d21
reedee777-art <reedee777@gmail.com>
2026-09-19 12:16:04 +0300

    Update script.user13.js

--- script.user13.js
+++ script.user13.js
@@ -24,7 +24,7 @@
         'faaset.com',
         'cryptogem.space',
         'mooncrypto.space',
-        'freeflarcrypto.com',
+        //'freeflarcrypto.com',
         'bigmobfaucet.com',
         'bitbitflow.online',
         'dogecoindrip.me',
